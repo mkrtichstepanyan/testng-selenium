95174b7c64f2c6b0a57206e961fa1144459f8229
garik <garik.garibyan.1990@gmail.com>
2023-11-27 18:54:09 +0400

    add homa page fields

--- src/main/java/org/example/pages/home/HomePage.java
+++ src/main/java/org/example/pages/home/HomePage.java
@@ -9,8 +9,38 @@ public class HomePage extends BasePage {
 
     private final HomeTopPanel homeTopPanel;
 
-    @FindBy(xpath = "//div//p[@id = 'homeButton']")
-    public WebElement homeButton;
+    @FindBy(xpath = "//div[@class = 'vlume-container screen-content']//div[@class = 'ng-star-inserted'][1]")
+    public WebElement recentlyAdded;
+
+    @FindBy(xpath = "//div[@class = 'vlume-container screen-content']//div[@class = 'ng-star-inserted'][2]")
+    public WebElement continueReading;
+
+    @FindBy(xpath = "//div[@class = 'vlume-container screen-content']//div[@class = 'ng-star-inserted'][3]")
+    public WebElement freeThisMonth;
+
+    @FindBy(xpath = "//div[@class = 'vlume-container screen-content']//div[@class = 'ng-star-inserted'][4]")
+    public WebElement trendingNow;
+
+    @FindBy(xpath = "//div[@class = 'vlume-container screen-content']//div[@class = 'ng-star-inserted'][5]")
+    public WebElement ourTopPicks;
+
+    @FindBy(xpath = "//div[@class = 'vlume-container screen-content']//div[@class = 'ng-star-inserted'][6]")
+    public WebElement newReleases;
+
+    @FindBy(xpath = "//div[@class = 'vlume-container screen-content']//div[@class = 'ng-star-inserted'][7]")
+    public WebElement mostRead;
+
+    @FindBy(xpath = "//div[@class = 'vlume-container screen-content']//div[@class = 'ng-star-inserted'][8]")
+    public WebElement topFiction;
+
+    @FindBy(xpath = "//div[@class = 'vlume-container screen-content']//div[@class = 'ng-star-inserted'][9]")
+    public WebElement topNonFiction;
+
+    @FindBy(xpath = "//div[@class = 'vlume-container screen-content']//div[@class = 'ng-star-inserted'][10]")
+    public WebElement antares;
+
+    @FindBy(xpath = "//div[@class = 'vlume-container screen-content']//div[@class = 'ng-star-inserted'][11]")
+    public WebElement zangak;
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -23,6 +53,6 @@ public class HomePage extends BasePage {
 
     @Override
     public boolean isPageOpened(WebElement... elements) {
-        return super.isPageOpened(homeButton);
+        return super.isPageOpened(recentlyAdded, continueReading, freeThisMonth, trendingNow, ourTopPicks, newReleases, mostRead, topFiction, topNonFiction, antares, zangak);
     }
 }
