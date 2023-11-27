dbe813f5fd02f8358b86e760f140080f24e9da2d
garik <garik.garibyan.1990@gmail.com>
2023-11-27 16:28:34 +0400

    set welcomeTopPanel is private

--- src/main/java/org/example/pages/wordpress/WelcomePage.java
+++ src/main/java/org/example/pages/wordpress/WelcomePage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.FindBy;
 
 public class WelcomePage extends BasePage {
 
-    WelcomeTopPanel welcomeTopPanel;
+    private final WelcomeTopPanel welcomeTopPanel;
 
     @FindBy(xpath = "//a[@href = 'https://dev.vlume.com/sign-in']")
     public WebElement signInButton;
