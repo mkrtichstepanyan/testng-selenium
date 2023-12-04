0f2ee184c88bead45710f4cc02385e6dea3edfc1
garik <garik.garibyan.1990@gmail.com>
2023-12-04 18:53:51 +0400

    add dependency assertj

--- src/test/java/settings/readingLanguage/ReadingLanguagePositiveTests.java
+++ src/test/java/settings/readingLanguage/ReadingLanguagePositiveTests.java
@@ -5,6 +5,7 @@ import dataProvider.url.UrlsProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.example.pages.settings.SettingsPage;
 import org.example.pages.settings.mat_card.ReadingLanguagesPage;
+import org.openqa.selenium.support.Color;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -33,10 +34,37 @@ public class ReadingLanguagePositiveTests extends AuthorizedTest {
     @Test
     public void verifyAllButtonIsSelected(){
         log.info("Assert radio button reading English is selected");
-        Assert.assertTrue(readingLanguagesPage.buttonReadEnglish.isSelected());
 
-        log.info("Assert radio button reading Armenian is selected");
-        Assert.assertTrue(readingLanguagesPage.buttonReadArmenia.isSelected());
+        String en = readingLanguagesPage.buttonReadEnglish.getCssValue("color");
+        // convert rgba to hex
+        String c = Color.fromString(en).asHex();
+        System.out.println("Color is :" + en);
+        System.out.println("Hex code for color:" + c);
+        driver.quit();
+
+        readingLanguagesPage.buttonReadEnglish.click();
+        String arm = readingLanguagesPage.buttonReadEnglish.getCssValue("color");
+        // convert rgba to hex
+        String cArm = Color.fromString(arm).asHex();
+        System.out.println("Color is :" + arm);
+        System.out.println("Hex code for color:" + cArm);
+        driver.quit();
+
+//        String background = readingLanguagesPage.buttonReadEnglish.getAttribute("background");
+//        String background_color = readingLanguagesPage.buttonReadEnglish.getAttribute("background-color");
+//        String background_color_css = readingLanguagesPage.buttonReadEnglish.getCssValue("background-color");
+//        String color = readingLanguagesPage.buttonReadEnglish.getCssValue("color");
+//        String style = readingLanguagesPage.buttonReadEnglish.getAttribute("style");
+//        String type = readingLanguagesPage.buttonReadEnglish.getAttribute("type");
+//        log.info("background: " + background);
+//        log.info(background_color);
+//        log.info(color);
+//        log.info(style);
+//        log.info(background_color_css);
+//        log.info(type);
+//        String hex = Color.fromString(color).asHex();
+//        log.info(hex);
+
     }
 
 }
